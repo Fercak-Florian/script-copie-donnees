899767490eef5e19f4921a58833ef7a3bc7a3da7
Florian Fercak <fercak.florian@gmail.com>
2026-06-09 13:52:32 +0200

    copy app data into separated file

--- copy-data.ps1
+++ copy-data.ps1
@@ -14,6 +14,7 @@ Import-Module "$PSScriptRoot\DisplayUser.psm1"
 Import-Module "$PSScriptRoot\Test-InputParameters.psm1"
 Import-Module "$PSScriptRoot\ConvertTo-DriveRoot.psm1"
 Import-Module "$PSScriptRoot\Copy-UserData.psm1"
+Import-Module "$PSScriptRoot\Copy-AppData.psm1"
 
 
 $robocopyOptions = @(
@@ -23,131 +24,6 @@ $robocopyOptions = @(
     '/r:3',
     '/mt:16'
 )
-
-# function Copy-User-Data {
-#     param (
-#         [Parameter(Position = 0)]
-#         [string]$disqueSource,
-#         [Parameter(Position = 1)]
-#         [string]$disqueDestination,
-#         [Parameter(Position = 2)]
-#         [string]$cuid
-#     )
-#     Write-Host "---- Debut de la copie des donnees Utilisateur ----" -ForegroundColor green
-#     Get-ChildItem -Path ${disqueSource}Users\${cuid} | Where-Object {
-#         $_.Name -notin @("!!!SvgClesZC!!!", "AppData", "Documents", "outlook") -and
-#         $_.Name -notlike "OneDrive*"
-#     } | ForEach-Object {
-#         $source = "${disqueSource}Users\${cuid}\$_"
-#         $destination = "${disqueDestination}Users\${cuid}\$_"
-#         robocopy $source $destination @robocopyOptions
-#     }
-
-#     Write-Host "---- Copie du repertoire Outlook presentes dans CUID\Documents\Outlook ----"
-#     Get-ChildItem -Path "${disqueSource}Users\${cuid}\Documents\Outlook" |
-#     Where-Object {
-#         $_.Name -notlike "*.com.ost" -and $_.Name -notlike "*.ost"
-#     } | ForEach-Object {
-#         $source = "${disqueSource}Users\${cuid}\Documents\Outlook"
-#         $destination = "${disqueDestination}Users\${cuid}\Outlook"
-#         robocopy $source $destination $_.Name
-#     }
-
-#     Write-Host "---- Copie du repertoire Outlook presentes dans CUID\Outlook ----"
-#     Get-ChildItem -Path "${disqueSource}Users\${cuid}\Outlook" |
-#     Where-Object {
-#         $_.Name -notlike "*.com.ost" -and $_.Name -notlike "*.ost"
-#     } | ForEach-Object {
-#         $source = "${disqueSource}Users\${cuid}\Outlook"
-#         $destination = "${disqueDestination}Users\${cuid}\Outlook"
-#         robocopy $source $destination $_.Name
-#     }
-
-#     Write-Host "---- Copie du repertoire Documents ----"
-#     $source = "${disqueSource}Users\${cuid}\Documents"
-#     $destination = "${disqueDestination}Users\${cuid}\Documents"
-#     robocopy $source $destination /xd Outlook @robocopyOptions
-
-#     Write-Host "---- Fin de Copie des donnees Utilisateur ----" -ForegroundColor green
-# }
-
-function Copy-App-Data {
-    param (
-        [Parameter(Position = 0)]
-        [string]$disqueSource,
-        [Parameter(Position = 1)]
-        [string]$disqueDestination,
-        [Parameter(Position = 2)]
-        [string]$cuid
-    )
-    Write-Host "---- Debut de la copie des donnees des applications ----" -ForegroundColor Yellow
-
-    Write-Host "---- Copie des donnees dans AppData\Roaming\Microsoft ----"
-    $data = @(
-        'Excel',
-        'Internet Explorer',
-        'OneNote',
-        'Outlook',
-        'Signatures',
-        'Teams',
-        'Templates',
-        'Word')
-
-    foreach ($element in $data) {
-        $source = "${disqueSource}Users\${cuid}\AppData\Roaming\Microsoft\$($element)"
-        $destination = "${disqueDestination}Users\${cuid}\AppData\Roaming\Microsoft\$($element)"
-        robocopy $source $destination @robocopyOptions
-    }
-
-    Write-Host "---- Copie des donnees en cache dans AppData\Roaming ----" 
-    $source = "${disqueSource}Users\${cuid}\AppData\Roaming"
-    $destination = "${disqueDestination}Users\${cuid}\AppData\Roaming"
-    robocopy $source $destination @robocopyOptions `
-        /xd com.adobe.dunamis connecteddevicesplatform ICAClient Microsoft packages "PRIM'X" vlc Xerox ZoneCentral
-
-    Write-Host "---- Copie de StickyNotes et PowerBI ----"
-    $apps = @("*StickyNotes*", "*PowerBI*")
-
-    Get-ChildItem "${disqueSource}Users\${cuid}\AppData\Local\Packages" -Directory |
-    Where-Object {
-        $name = $_.Name
-        $apps | Where-Object { $name -like $_ }
-    } |
-    ForEach-Object {
-        $source = "${disqueSource}Users\${cuid}\AppData\Local\Packages\$($PSItem)"
-        $destination = "${disqueDestination}Users\${cuid}\AppData\Local\Packages\$($PSItem)"
-        robocopy $source $destination @robocopyOptions
-    }
-
-    # Dans AppData\Local\Microsoft copie de tous les repertoires Edge, IE, OneNote, Outlook, Teams
-
-    Write-Host "---- Copie des donnees Microsoft en cache dans AppData\Local\Microsoft\Edge ----"
-    Get-ChildItem "${disqueSource}Users\${cuid}\AppData\Local\Microsoft" -Directory -Filter "Edge*" | ForEach-Object {
-        $source = "${disqueSource}Users\${cuid}\AppData\Local\Microsoft\$($PSItem)"
-        $destination = "${disqueDestination}Users\${cuid}\AppData\Local\Microsoft\$($PSItem)"
-        robocopy $source $destination @robocopyOptions
-    }
-
-    Write-Host "---- Copie des donnees Microsoft en cache dans AppData\Local\Microsoft\Internet Explorer ----"
-    Get-ChildItem "${disqueSource}Users\${cuid}\AppData\Local\Microsoft" -Directory -Filter "Internet Explorer" |
-    ForEach-Object {
-        $source = $_.FullName
-        $destination = "${disqueDestination}Users\${cuid}\AppData\Local\Microsoft\$($_.Name)"
-        robocopy $source $destination @robocopyOptions
-    }
-
-    Write-Host "---- Copie des donnees Microsoft en cache dans .\AppData\Local\Microsoft\OneNote ----"
-    robocopy "${disqueSource}Users\${cuid}\AppData\Local\Microsoft\OneNote" "${disqueDestination}Users\${cuid}\AppData\Local\Microsoft\OneNote" @robocopyOptions
-    robocopy "${disqueSource}Users\${cuid}\AppData\Local\Microsoft\Outlook" "${disqueDestination}Users\${cuid}\AppData\Local\Microsoft\Outlook" @robocopyOptions
-
-    Write-Host "---- Copie des donnees Microsoft en cache dans .\AppData\Local\Microsoft\Teams ----"
-    Get-ChildItem "${disqueSource}Users\${cuid}\AppData\Local\Microsoft" -Directory -Filter "Teams*" | ForEach-Object {
-        $source = "${disqueSource}Users\${cuid}\AppData\Local\Microsoft\$($PSItem)"
-        $destination = "${disqueDestination}Users\${cuid}\AppData\Local\Microsoft\$($PSItem)"
-        robocopy $source $destination @robocopyOptions
-    }
-    Write-Host "---- Fin de la copie des donnees des applications ----" -ForegroundColor green
-}
 
 function main {
     param(
@@ -170,7 +46,7 @@ function main {
     robocopy "${sourceDisk}Applications" "${targetDisk}Applications" @robocopyOptions
     robocopy "${sourceDisk}My Program Files" "${targetDisk}My Program Files" @robocopyOptions
     Copy-UserData $sourceDisk $targetDisk $cuid
-    Copy-App-Data $sourceDisk $targetDisk $cuid
+    Copy-AppData $sourceDisk $targetDisk $cuid
     Write-Host "La copie des donnees est terminee" -ForegroundColor Green
 }
 
